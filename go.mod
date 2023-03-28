ec1b7fb7ad4d0f4791cfd797ad4468f11b2dc5d9
scorpiotzh <835598264@qq.com>
2023-03-28 17:15:03 +0800

    feat: Add parser_tron

--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.17
 
 require (
 	github.com/btcsuite/btcd v0.23.0
-	github.com/dotbitHQ/das-lib v1.0.1-0.20230322075702-a725ca201ef7
+	github.com/dotbitHQ/das-lib v1.0.1-0.20230328085145-d0e45e02f8d0
 	github.com/fsnotify/fsnotify v1.4.9
 	github.com/gin-gonic/gin v1.7.2
 	github.com/nervosnetwork/ckb-sdk-go v0.101.3
@@ -12,7 +12,7 @@ require (
 	github.com/scorpiotzh/mylog v1.0.10
 	github.com/scorpiotzh/toolib v1.1.6
 	github.com/shopspring/decimal v1.3.1
-	github.com/urfave/cli/v2 v2.3.0
+	github.com/urfave/cli/v2 v2.10.2
 	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c
 	gorm.io/gorm v1.23.6
 )
@@ -20,18 +20,18 @@ require (
 require (
 	github.com/Andrew-M-C/go.emoji v1.0.1 // indirect
 	github.com/StackExchange/wmi v0.0.0-20180116203802-5d049714c4a6 // indirect
-	github.com/btcsuite/btcd/btcec/v2 v2.1.3 // indirect
+	github.com/btcsuite/btcd/btcec/v2 v2.2.0 // indirect
 	github.com/btcsuite/btcd/btcutil v1.1.3 // indirect
 	github.com/btcsuite/btcd/chaincfg/chainhash v1.0.1 // indirect
 	github.com/btcsuite/btclog v0.0.0-20170628155309-84c8d2346e9f // indirect
 	github.com/btcsuite/btcutil v0.0.0-20190425235716-9e5f4b9a998d // indirect
 	github.com/clipperhouse/uax29 v1.12.4 // indirect
-	github.com/cpuguy83/go-md2man/v2 v2.0.0-20190314233015-f79a8a8ca69d // indirect
+	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
 	github.com/deckarep/golang-set v1.8.0 // indirect
 	github.com/decred/dcrd/crypto/blake256 v1.0.0 // indirect
 	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.0.1 // indirect
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
-	github.com/ethereum/go-ethereum v1.10.17 // indirect
+	github.com/ethereum/go-ethereum v1.10.26 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-ole/go-ole v1.2.1 // indirect
 	github.com/go-playground/locales v0.13.0 // indirect
@@ -51,21 +51,22 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.1 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
-	github.com/russross/blackfriday/v2 v2.0.1 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/shirou/gopsutil v3.21.4-0.20210419000835-c7a38de76ee5+incompatible // indirect
-	github.com/shurcooL/sanitized_anchor_name v1.0.0 // indirect
+	github.com/smartystreets/goconvey v1.7.2 // indirect
 	github.com/tklauser/go-sysconf v0.3.5 // indirect
 	github.com/tklauser/numcpus v0.2.2 // indirect
 	github.com/tron-us/go-common v1.0.2 // indirect
 	github.com/ugorji/go/codec v1.1.7 // indirect
+	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
 	go.uber.org/atomic v1.7.0 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
 	go.uber.org/zap v1.18.1 // indirect
-	golang.org/x/crypto v0.0.0-20210322153248-0c34fe9e7dc2 // indirect
-	golang.org/x/net v0.0.0-20210805182204-aaa1db679c0d // indirect
-	golang.org/x/sys v0.0.0-20210816183151-1e6c022a8912 // indirect
-	golang.org/x/text v0.3.7 // indirect
-	google.golang.org/protobuf v1.26.0 // indirect
+	golang.org/x/crypto v0.7.0 // indirect
+	golang.org/x/net v0.8.0 // indirect
+	golang.org/x/sys v0.6.0 // indirect
+	golang.org/x/text v0.8.0 // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
 	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
 	gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df // indirect
 	gopkg.in/natefinch/lumberjack.v2 v2.0.0 // indirect
---
+++ parser/parser_tron/parser_tron.go
@@ -0,0 +1,262 @@
+package parser_tron
+
+import (
+	"encoding/hex"
+	"fmt"
+	"github.com/dotbitHQ/das-lib/chain/chain_tron"
+	"github.com/dotbitHQ/unipay/parser/parser_common"
+	"github.com/dotbitHQ/unipay/tables"
+	"github.com/fbsobreira/gotron-sdk/pkg/proto/api"
+	"github.com/fbsobreira/gotron-sdk/pkg/proto/core"
+	"github.com/golang/protobuf/proto"
+	"github.com/scorpiotzh/mylog"
+	"github.com/shopspring/decimal"
+	"golang.org/x/sync/errgroup"
+	"sync"
+	"sync/atomic"
+	"time"
+)
+
+var log = mylog.NewLogger("parser_tron", mylog.LevelDebug)
+
+type ParserTron struct {
+	parser_common.ParserCommon
+	ChainTron *chain_tron.ChainTron
+}
+
+func (p *ParserTron) getLatestBlockNumber() (uint64, error) {
+	currentBlockNumber, err := p.ChainTron.GetBlockNumber()
+	if err != nil {
+		return 0, fmt.Errorf("GetBlockNumber err: %s", err.Error())
+	}
+	return uint64(currentBlockNumber), nil
+}
+
+func (p *ParserTron) Parser() {
+	if err := p.InitCurrentBlockNumber(p.getLatestBlockNumber); err != nil {
+		log.Error("InitCurrentBlockNumber err: ", err.Error())
+		return
+	}
+	atomic.AddUint64(&p.CurrentBlockNumber, 1)
+	p.Wg.Add(1)
+	for {
+		select {
+		default:
+			latestBlockNumber, err := p.getLatestBlockNumber()
+			if err != nil {
+				log.Error("getLatestBlockNumber err: ", err.Error())
+				time.Sleep(time.Second * 10)
+			} else if p.ConcurrencyNum > 1 && p.CurrentBlockNumber < (latestBlockNumber-p.ConfirmNum-p.ConcurrencyNum) {
+				nowTime := time.Now()
+				if err := p.parserConcurrencyMode(); err != nil {
+					log.Error("parserConcurrencyMode err:", p.ParserType, err.Error(), p.CurrentBlockNumber)
+				}
+				log.Warn("parserConcurrencyMode time:", p.ParserType, time.Since(nowTime).Seconds())
+				time.Sleep(time.Second * 1)
+			} else if p.CurrentBlockNumber < (latestBlockNumber - p.ConfirmNum) {
+				nowTime := time.Now()
+				if err := p.parserSubMode(); err != nil {
+					log.Error("parserSubMode err:", p.ParserType, err.Error(), p.CurrentBlockNumber)
+				}
+				log.Warn("parserSubMode time:", p.ParserType, time.Since(nowTime).Seconds())
+				time.Sleep(time.Second * 5)
+			} else {
+				log.Info("Parser:", p.ParserType, p.CurrentBlockNumber, latestBlockNumber)
+				time.Sleep(time.Second * 10)
+			}
+		case <-p.Ctx.Done():
+			p.Wg.Done()
+			return
+		}
+	}
+}
+
+func (p *ParserTron) parsingBlockData(block *api.BlockExtention) error {
+	if block == nil {
+		return fmt.Errorf("block is nil")
+	}
+	for _, tx := range block.Transactions {
+		if len(tx.Transaction.RawData.Contract) != 1 {
+			continue
+		}
+		orderId := chain_tron.GetMemo(tx.Transaction.RawData.Data)
+		if orderId == "" {
+			continue
+		} else if len(orderId) > 64 {
+			continue
+		}
+
+		switch tx.Transaction.RawData.Contract[0].Type {
+		case core.Transaction_Contract_TransferContract:
+			instance := core.TransferContract{}
+			if err := proto.Unmarshal(tx.Transaction.RawData.Contract[0].Parameter.Value, &instance); err != nil {
+				log.Error(" proto.Unmarshal err:", err.Error())
+				continue
+			}
+			fromAddr, toAddr := hex.EncodeToString(instance.OwnerAddress), hex.EncodeToString(instance.ToAddress)
+			if toAddr != p.Address {
+				continue
+			}
+			log.Info("parsingBlockData orderId:", orderId, hex.EncodeToString(tx.Txid))
+
+			// check order id
+			order, err := p.DbDao.GetOrderInfoByOrderId(orderId)
+			if err != nil {
+				return fmt.Errorf("GetOrderInfoByOrderId err: %s", err.Error())
+			} else if order.Id == 0 {
+				log.Warn("GetOrderInfoByOrderId is not exist:", p.ParserType, orderId)
+				continue
+			}
+			if order.PayTokenId != p.PayTokenId {
+				log.Warn("order pay token id not match", order.OrderId)
+				continue
+			}
+
+			amountValue := decimal.New(instance.Amount, 0)
+			if amountValue.Cmp(order.Amount) == -1 {
+				log.Warn("tx value less than order amount:", amountValue.String(), order.Amount.String())
+				continue
+			}
+			// change the status to confirm
+			paymentInfo := tables.TablePaymentInfo{
+				Id:            0,
+				PayHash:       hex.EncodeToString(tx.Txid),
+				OrderId:       order.OrderId,
+				PayAddress:    fromAddr,
+				AlgorithmId:   order.AlgorithmId,
+				Timestamp:     tx.Transaction.RawData.Timestamp,
+				Amount:        order.Amount,
+				PayHashStatus: tables.PayHashStatusConfirm,
+				RefundStatus:  tables.RefundStatusDefault,
+				RefundHash:    "",
+				RefundNonce:   0,
+			}
+			if err := p.DbDao.UpdatePaymentStatus(paymentInfo); err != nil {
+				return fmt.Errorf("UpdatePaymentStatus err: %s", err.Error())
+			}
+		case core.Transaction_Contract_TransferAssetContract:
+		case core.Transaction_Contract_TriggerSmartContract:
+		}
+	}
+	return nil
+}
+
+func (p *ParserTron) parserSubMode() error {
+	log.Info("parserSubMode:", p.ParserType, p.CurrentBlockNumber)
+
+	block, err := p.ChainTron.GetBlockByNumber(p.CurrentBlockNumber)
+	if err != nil {
+		return fmt.Errorf("GetBlockByNumber err: %s", err.Error())
+	}
+	if block.BlockHeader == nil {
+		return fmt.Errorf("parserSubMode: block.BlockHeader is nil[%d]", p.CurrentBlockNumber)
+	} else if block.BlockHeader.RawData == nil {
+		return fmt.Errorf("parserSubMode: block.BlockHeader.RawData is nil[%d]", p.CurrentBlockNumber)
+	}
+
+	blockHash := hex.EncodeToString(block.Blockid)
+	parentHash := hex.EncodeToString(block.BlockHeader.RawData.ParentHash)
+	log.Info("parserSubMode:", p.ParserType, blockHash, parentHash)
+
+	if fork, err := p.CheckFork(parentHash); err != nil {
+		return fmt.Errorf("CheckFork err: %s", err.Error())
+	} else if fork {
+		log.Warn("CheckFork is true:", p.ParserType, p.CurrentBlockNumber, blockHash, parentHash)
+		if err := p.DbDao.DeleteBlockInfoByBlockNumber(p.ParserType, p.CurrentBlockNumber-1); err != nil {
+			return fmt.Errorf("DeleteBlockInfoByBlockNumber err: %s", err.Error())
+		}
+		atomic.AddUint64(&p.CurrentBlockNumber, ^uint64(0))
+	} else if err := p.parsingBlockData(block); err != nil {
+		return fmt.Errorf("parsingBlockData err: %s", err.Error())
+	} else {
+		blockInfo := tables.TableBlockParserInfo{
+			ParserType:  p.ParserType,
+			BlockNumber: p.CurrentBlockNumber,
+			BlockHash:   blockHash,
+			ParentHash:  parentHash,
+		}
+		if err = p.DbDao.CreateBlockInfo(blockInfo); err != nil {
+			return fmt.Errorf("CreateBlockInfo err: %s", err.Error())
+		} else {
+			atomic.AddUint64(&p.CurrentBlockNumber, 1)
+		}
+		if err = p.DbDao.DeleteBlockInfo(p.ParserType, p.CurrentBlockNumber-20); err != nil {
+			return fmt.Errorf("DeleteBlockInfo err: %s", err.Error())
+		}
+	}
+	return nil
+}
+
+func (p *ParserTron) parserConcurrencyMode() error {
+	log.Info("parserConcurrencyMode:", p.ParserType, p.CurrentBlockNumber, p.ConcurrencyNum)
+
+	var blockList = make([]tables.TableBlockParserInfo, p.ConcurrencyNum)
+	var blocks = make([]*api.BlockExtention, p.ConcurrencyNum)
+	var blockCh = make(chan *api.BlockExtention, p.ConcurrencyNum)
+
+	blockLock := &sync.Mutex{}
+	blockGroup := &errgroup.Group{}
+
+	for i := uint64(0); i < p.ConcurrencyNum; i++ {
+		bn := p.CurrentBlockNumber + i
+		index := i
+		blockGroup.Go(func() error {
+			block, err := p.ChainTron.GetBlockByNumber(bn)
+			if err != nil {
+				return fmt.Errorf("GetBlockByNumber err:%s [%d]", err.Error(), bn)
+			}
+			if block.BlockHeader == nil {
+				return fmt.Errorf("parserSubMode: block.BlockHeader is nil[%d]", bn)
+			} else if block.BlockHeader.RawData == nil {
+				return fmt.Errorf("parserSubMode: block.BlockHeader.RawData is nil[%d]", bn)
+			}
+
+			hash := hex.EncodeToString(block.Blockid)
+			parentHash := hex.EncodeToString(block.BlockHeader.RawData.ParentHash)
+
+			blockLock.Lock()
+			blockList[index] = tables.TableBlockParserInfo{
+				ParserType:  p.ParserType,
+				BlockNumber: bn,
+				BlockHash:   hash,
+				ParentHash:  parentHash,
+			}
+			blocks[index] = block
+			blockLock.Unlock()
+
+			return nil
+		})
+	}
+	if err := blockGroup.Wait(); err != nil {
+		return fmt.Errorf("errGroup.Wait()1 err: %s", err.Error())
+	}
+
+	for i := range blocks {
+		blockCh <- blocks[i]
+	}
+	close(blockCh)
+
+	blockGroup.Go(func() error {
+		for v := range blockCh {
+			if err := p.parsingBlockData(v); err != nil {
+				return fmt.Errorf("parsingBlockData err: %s", err.Error())
+			}
+		}
+		return nil
+	})
+
+	if err := blockGroup.Wait(); err != nil {
+		return fmt.Errorf("errGroup.Wait()2 err: %s", err.Error())
+	}
+
+	// block
+	if err := p.DbDao.CreateBlockInfoList(blockList); err != nil {
+		return fmt.Errorf("CreateBlockInfoList err:%s", err.Error())
+	} else {
+		atomic.AddUint64(&p.CurrentBlockNumber, p.ConcurrencyNum)
+	}
+	if err := p.DbDao.DeleteBlockInfo(p.ParserType, p.CurrentBlockNumber-20); err != nil {
+		log.Error("DeleteBlockInfo err:", p.ParserType, err.Error())
+	}
+	return nil
+}
