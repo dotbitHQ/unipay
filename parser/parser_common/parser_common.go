116ae35a8171f619414cc615723d9b2c54a09d34
scorpiotzh <835598264@qq.com>
2023-05-29 20:33:45 +0800

    feat: Modify lark notify err msg

--- parser/parser_common/parser_common.go
+++ parser/parser_common/parser_common.go
@@ -76,7 +76,7 @@ func (p *ParserCommon) Parser() {
 				nowTime := time.Now()
 				if err := p.PA.SingleParsing(p.PC); err != nil {
 					log.Error("SingleParsing err:", parserType, err.Error(), p.PC.CurrentBlockNumber)
-					if !strings.Contains(err.Error(), "data is nil") {
+					if !strings.Contains(err.Error(), "data is nil") || !strings.Contains(err.Error(), "HTTP status code received from server: 503") {
 						notify.SendLarkTextNotify(config.Cfg.Notify.LarkErrorKey, fmt.Sprintf("Parser %d", parserType), err.Error())
 					}
 				}
